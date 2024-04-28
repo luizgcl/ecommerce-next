230ba64f81cdea1b39d84ed0156004cdfc499d13
luizgcl <luiizgcl@gmail.com>
2024-04-28 02:53:12 -0300

    feat: add second discount banner to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -25,9 +25,18 @@ export default async function Home() {
       <div className="mt-8 px-5">
         <Categories />
       </div>
-      <div className="mt-8">
+      <div className="mt-8 mb-3">
+        <p className="font-bold uppercase pl-5 mb-3">Ofertas</p>
         <ProductList products={deals} />
       </div>
+      <Image
+        src="/banner-home-02.png"
+        width={0}
+        height={0}
+        className="h-auto w-full px-5"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses"
+      />
     </main>
   );
 }
