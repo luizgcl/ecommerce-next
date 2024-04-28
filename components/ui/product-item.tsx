f05c045ae02c29d3b7b1fd3f90861c47f6959f1a
luizgcl <luiizgcl@gmail.com>
2024-04-28 20:58:05 -0300

    feat: ensure user goes to product details page when click on product item

--- components/ui/product-item.tsx
+++ components/ui/product-item.tsx
@@ -2,6 +2,7 @@ import { ProductWithTotalPrice } from '@/helpers/product'
 import Image from "next/image";
 import { Badge } from './badge';
 import { ArrowDown, ArrowDownIcon } from 'lucide-react';
+import Link from 'next/link';
 
 interface ProductItemProps {
   product: ProductWithTotalPrice
@@ -9,56 +10,58 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="relative bg-accent rounded-lg w-full h-[170px] flex items-center">
-        <Image
-          src={product.imageUrls[0]}
-          height={0}
-          width={0}
-          sizes="100vw"
-          className="h-auto w-auto max-w-[80%] max-h-[70%]"
-          style={{
-            objectFit: "contain"
-          }}
-          alt={product.name}
-        />
+    <Link href={`/product/${product.slug}`}>
+      <div className="flex flex-col gap-4">
+        <div className="relative bg-accent rounded-lg w-full h-[170px] flex items-center">
+          <Image
+            src={product.imageUrls[0]}
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-auto max-w-[80%] max-h-[70%]"
+            style={{
+              objectFit: "contain"
+            }}
+            alt={product.name}
+          />
 
-        {product.discountPercentage > 0 && (
-          <Badge className="absolute left-3 top-3 px-1 py-1/2">
-            <ArrowDownIcon size={14} />
-            {product.discountPercentage}%
-          </Badge>
-        )}
-      </div>
+          {product.discountPercentage > 0 && (
+            <Badge className="absolute left-3 top-3 px-1 py-1/2">
+              <ArrowDownIcon size={14} />
+              {product.discountPercentage}%
+            </Badge>
+          )}
+        </div>
 
-      <div className='flex flex-col gap-1'>
-        <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">
-          {product.name}
-        </p>
+        <div className='flex flex-col gap-1'>
+          <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">
+            {product.name}
+          </p>
 
-        <div className="flex items-center gap-2">
-          {product.discountPercentage > 0 ? (
-            <>
-              <p className='font-semibold'>{product.totalPrice.toLocaleString('pt-BR', {
+          <div className="flex items-center gap-2">
+            {product.discountPercentage > 0 ? (
+              <>
+                <p className='font-semibold'>{product.totalPrice.toLocaleString('pt-BR', {
+                  currency: 'BRL',
+                  style: 'currency'
+                })}</p>
+                <p className='opacity-75 line-through text-xs'>{
+                  Number(product.basePrice).toLocaleString('pt-BR', {
+                    currency: 'BRL',
+                    style: 'currency'
+                  })
+                }</p>
+              </>
+            ) : (
+              <p className='font-semibold text-sm'>{product.totalPrice.toLocaleString('pt-BR', {
                 currency: 'BRL',
                 style: 'currency'
               })}</p>
-              <p className='opacity-75 line-through text-xs'>{
-                Number(product.basePrice).toLocaleString('pt-BR', {
-                  currency: 'BRL',
-                  style: 'currency'
-                })
-              }</p>
-            </>
-          ) : (
-            <p className='font-semibold text-sm'>{product.totalPrice.toLocaleString('pt-BR', {
-              currency: 'BRL',
-              style: 'currency'
-            })}</p>
-          )}
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
