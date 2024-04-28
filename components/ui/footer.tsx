3bd5964e52a4264c3641371ab394bb5b74b2eb26
luizgcl <luiizgcl@gmail.com>
2024-04-28 03:21:54 -0300

    feat: add footer component

---
+++ components/ui/footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return <footer className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+    &copy; 2023 Copyright <span className="font-semibold">FSW Store</span>
+  </footer>
+}
+
+export default Footer;
